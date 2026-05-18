9a1b5b5810ffb3a495d1b182fec5b2ec4e6df11e
Yashwanth <yashwanth.r@shellkode.com>
2026-05-18 19:05:31 +0530

    fix model id

--- reviewer/main.py
+++ reviewer/main.py
@@ -18,14 +18,6 @@ bedrock = boto3.client(
     region_name="us-east-1"
 )
 
-# @app.post("/webhook")
-# async def github_webhook(request: Request):
-#     try:
-#         payload = await request.json()
-#     except Exception:
-#         return {"message": "Invalid payload"}
-
-#     print(f"Action received: {payload.get('action')}")
 
 @app.post("/webhook")
 async def github_webhook(request: Request):
